a0d62bf56e5ea6a26aed3325ef38f1605d9cd7b3
BryanBogran <bogranbryan@gmail.com>
2026-01-10 19:01:08 -0600

    Changed to dark mode and added "Alarms" and Add symbol

--- Alarm app/ContentView.swift
+++ Alarm app/ContentView.swift
@@ -19,27 +19,55 @@ struct ContentView: View {
     ]
     
     var body: some View {
-        ForEach($alarms) { $alarm in
-            ZStack {
-                Color(.white)
+        ZStack {
+            Color(red: 24/255.0, green: 24/255.0, blue: 24/255.0)
                 .ignoresSafeArea()
-                
+            VStack {
                 HStack {
-                VStack(alignment: .leading, spacing: 2.0) {
-                    Text("7:30")
-                            .font(.system(size: 50, weight: .regular))
-                        Text(alarm.label)
-                    }
+                    Text("Alarms")
+                        .font(.system(size: 30, weight: .bold))
+                        .foregroundStyle(.white)
                     Spacer()
-                    Toggle("", isOn: $alarm.isEnabled)
+                    
+                    Image(systemName: "plus.circle.fill")
+                        .font(.system(size: 30))
+                        .foregroundStyle(.white)
                 }
-                .padding(10)
-                .background(Rectangle()
-                    .foregroundStyle(.white)
-                    .cornerRadius(10)
-                    .shadow(radius: 5))
-                .padding(5)
+                .padding(.horizontal, 15)
+                ForEach($alarms) { $alarm in
+                    HStack {
+                        VStack(alignment: .leading, spacing: 2.0) {
+                            
+                            (
+                            Text("7:30")
+                                .font(.system(size: 60, weight: .regular))
+                            +
+                            Text(" AM")
+                                .font(.system(size: 30, weight: .regular))
+                                .baselineOffset(8)
+                            )
+                                .foregroundStyle(.white)
+                            Text(alarm.label)
+                                .foregroundStyle(.white)
+                        }
+                        Spacer()
+                        Toggle("", isOn: $alarm.isEnabled)
+                    }
+                    .padding(10)
+                    .background(
+                        Rectangle()
+                            .foregroundStyle(Color(
+                                red: 44/255.0,
+                                green: 44/255.0,
+                                blue: 46/255.0))
+                            .cornerRadius(10)
+                            .shadow(radius: 5)
+                    )
+                    .padding(5)
+                }
+                Spacer()
             }
+            .padding(.top, 16)
         }
     }
 }
